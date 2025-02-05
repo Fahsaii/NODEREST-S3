db6eea186bdb304900a2699d14d76126eca08d7e
fahsaii <fahsai.tpd@gmail.com>
2025-02-05 08:08:56 +0700

    commit

--- index.js
+++ index.js
@@ -7,58 +7,57 @@ app.use(express.json());
 let books = [
     {
         id: 1,
-        title: 'book 1',
+        title: 'Book 1',
         author: 'Author 1'
     },
     {
         id: 2,
-        title: 'book 2',
+        title: 'Book 2',
         author: 'Author 2'
     },
     {
         id: 3,
-        title: 'book 3',
+        title: 'Book 3',
         author: 'Author 3'
-    },
+    }
 ];
 
-app.get('/books', (req, res)  => {
+app.get('/books', (req,res) => {
     res.json(books);
 });
 
-app.get('/books/:id', (req, res) => {
-    const bool = books.find(b => b.id === parseInt(req.params.id));
+app.get('/books/:id', (req,res) => {
+    const book = books.find(b => b.id === parseInt(req.params.id));
     if (!book) res.status(404).send('Book not found');
     res.json(book);
 });
 
-app.post('/books', (req, res) =>{
+app.post('/book', (req, res) => {
     const book = {
         id: books.length + 1,
-        title: req.body.title,
-        author: req.body.author
-    }
-    book.push(book);
+        title: req.body.author
+    };
+    books.push(book);
     res.send(book);
 });
-app.put('/books/:id', (req, res) => {
-  const book = books.find(b => b.id === parseInt(req.params.id));
-  if (!book) return res.status(404).send('Book not found');
 
-  book.title = req.body.title;
-  book.author = req.body.author;
-  res.send(book);
+// route to update a book
+app.put('/books/:id', (req, res) => {
+    const book = books.find(b => b.id === parseInt(req.params.id));
+    if (!book) res.status(404).send('Book not found');
+    book.title = req.body.title;
+    book.author = req.body.author;
+    res.send(book);
 });
 
-
+// route to delete a book
 app.delete('/books/:id', (req, res) => {
     const book = books.find(b => b.id === parseInt(req.params.id));
     if (!book) res.status(404).send('Book not found');
     const index = books.indexOf(book);
     books.splice(index, 1);
     res.send(book);
-  });
-  
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-  
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
